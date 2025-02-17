923ba99e28ab0ca10d8350f9e30fe5e0be1a8e7c
gabrielfernando01 <fernando.rozepeda@gmail.com>
2025-02-16 22:14:42 -0600

    create java-tutorial directory

---
+++ rock-the-jmv/java-tutorial-for-beginner/src/com/rockthejvm/Main.java
@@ -0,0 +1,11 @@
+package com.rockthejvm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I'm learning Java!");
+
+        // variables
+        int aNumber = 2;
+
+    }
+}
---
+++ rock-the-jmv/scala_at_light_speed/src/main/scala/com/rockthejvm/ObjectOrientation.scala
@@ -0,0 +1,36 @@
+package com.rockthejvm
+
+object ObjectOrientation extends App {
+
+  // class and instance
+  class Animal{
+
+    // define fields
+    val age: Int = 0
+    // define methods
+    def eat() = println("I'm eating")
+  }
+  val anAnimal = new Animal
+
+  // inheritance
+  class Dog(val name: String) extends Animal // constructor definition
+  val aDog = new Dog("Lassie")
+
+  // constructor arguments are NOT fields: need to put a val before the constructor argument
+  aDog.name
+
+  // subtype polymorphism
+  val aDeclaredAnimal: Animal = new Dog("Hichie")
+  aDeclaredAnimal.eat()   // the most derived method will be called at runtime
+
+  // abstract class
+  abstract class WalkingAnimal{
+    val hasLegs: Boolean = true   // by default public, can restrict by adding private or protected
+    def walk(): Unit
+  }
+
+  // "interface": ultimate abstract type
+  trait Carnivore{
+    def eat(animal: Animal): Unit
+  }
+}
