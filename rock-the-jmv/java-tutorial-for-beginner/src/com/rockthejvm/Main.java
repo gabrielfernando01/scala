1ca034a2f26138babc461897086c8791945da320
gabrielfernando01 <fernando.rozepeda@gmail.com>
2025-02-22 20:39:49 -0600

    update rock-the-jvm/basic-java directory

--- rock-the-jmv/java-tutorial-for-beginner/src/com/rockthejvm/Main.java
+++ rock-the-jmv/java-tutorial-for-beginner/src/com/rockthejvm/Main.java
@@ -58,8 +58,91 @@ public class Main {
         // boolean operations
         boolean numberIsTooBig = aNumber > 100; // evaluates to false
         boolean isLetterMyFavorite = aLetter == 'l'; // == means the COMPARISON EQUALITY operator
-        boolean smallOddNumber = (aNumber < 10) && (aNumber % 2 == 0);
+        boolean smallOddNumber = (aNumber < 10) && (aNumber % 2 == 0);  // && for logical AND, || for logical OR
+        boolean negate = !smallOddNumber;
 
+        // String operations
+        String concatenation = "I love " + "Java!"; // concatenates strings => "I love Java!"
+        System.out.println(concatenation);
+
+        // instructions
+        aNumber = 45;   // aNumber BECOMES 45
+        aNumber = aNumber + 6; // aNumber becomes 51
+        aNumber += 6; // equivalent
+        // old variables don't update automatically
+        System.out.println("sum is: " + sum);
+
+        // control statements
+        int age = 45;
+        if (age < 30) { // if/else statements
+            System.out.println("You're just getting started");
+            System.out.println("Something else!");
+        } else {
+            System.out.println("Perfect time to start learning Java");
+            age += 1;
+            System.out.println("You're age has become " + age + " years old");
+        }
+
+        // chain if-else statements
+        int temperature = 22;
+        if (temperature < 20) {
+            System.out.println("Quite chill outside, maybe take a jacket");
+        } else if (temperature < 25) {
+            System.out.println("Very pleasant outside, take a walk.");
+        } else {
+            System.out.println("It's getting hot, maybe take some water.");
+        }
+
+        // while loops: test conditions, then execute
+        System.out.println("Watch me cout to 10!");
+        int counter = 1;
+
+        while (counter <= 10) {
+            System.out.println(counter);
+            counter++;
+        }
+
+        // do-while loop: execute, then test condition
+        counter = 1;
+        do {
+            System.out.println(counter);
+            counter++;
+        } while (counter <= 10);
+
+        // for loops: iterate over a range
+        for (
+                int i = 1;  // starting instruction
+                i <= 10;    // continuation condition
+                i++         // instruction to execute AFTER the contents
+        ) {
+            System.out.println(i);  // contents to execute
+        }
+        /*
+            for structure operations (pseudo-code)
+
+            execute starting instruction;
+            as long as (continuation condition) {
+                run contents;
+                instruction to execute after the contents
+            }
+         */
+
+        // reference type
+        // instantiate: create a instance of a class
+        Person alice = new Person();    // alice is an INSTANCE of Person
+        alice.username = "Alice";
+        alice.age = 25;
+        Person bob = new Person(); // bob is an INSTANCE of Person
+        bob.username = "Bob";
+        bob.age = 24;
+        System.out.println(
+                alice.username + " say to " + bob.username + ": happy birthday for turning " + bob.age + "!"
+        );
     }
+}
 
+class Person {  // REFERENCES TYPE
+    // fields; pieces of information attached to a single type
+    String username;
+    int age;
 }
